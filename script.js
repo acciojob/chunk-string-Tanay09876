d0924c34ac17ab9afec5aa254190f7394ca9426b
Tanay09876 <152384332+Tanay09876@users.noreply.github.com>
2025-06-18 08:03:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function stringChop(str, size) {
-
   size = Number(size);
-  if (str === null || isNaN(size) || size <= 0) return [];
+
+  if (str === null || str === undefined || isNaN(size) || size <= 0) {
+    return [];
+  }
 
   const result = [];
   for (let i = 0; i < str.length; i += size) {
@@ -11,7 +13,7 @@ function stringChop(str, size) {
   return result;
 }
 
-
+// Do not change the code below
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
-alert(JSON.stringify(stringChop(str, size))); 
+alert(JSON.stringify(stringChop(str, size)));
