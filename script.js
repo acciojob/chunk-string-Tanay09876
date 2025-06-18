5d860b4ba5e7ea26cc56a9fc15c8df332fb0101f
Tanay09876 <152384332+Tanay09876@users.noreply.github.com>
2025-06-18 07:55:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function stringChop(str, size) {
-  // your code here
+ 
+  if (str === null || size === null || size <= 0) return [];
+
+  const result = [];
+
+  for (let i = 0; i < str.length; i += Number(size)) {
+    result.push(str.slice(i, i + Number(size)));
+  }
+
+  return result;
 }
 
-// Do not change the code below
+
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size));
+alert(JSON.stringify(stringChop(str, size)));
